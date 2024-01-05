21e70af38444ef62102995f3ec8e6e8b568d4ea7
Brisc Paul <alex_brisc@yahoo.com>
2024-01-05 17:15:38 +0200

    Functie afisare

--- main.cpp
+++ main.cpp
@@ -18,8 +18,33 @@ void plasare(int tabla[8][8])
 
 }
 
+void afisare(int tabla[8][8])
+{
+    char opt,i,j;
+    do{
+        
+        cout<<"Apasati tasta ENTER pentru a genera sau ESC pentru a iesi:"<<endl;
+        opt=_getch();
+        if(opt==13){
+        plasare(tabla);
+            for(i=0;i<8;i++)
+                {
+                    for(j=0;j<8;j++)
+                    {
+                        if(tabla[i][j]==1)
+                        cout<<'T'<<" ";
+                        else
+                        cout<<tabla[i][j]<<" ";
+                    }
+                    cout<<endl; 
+                }
+        }
+    }while(opt!=27);
+}
+
 int main()
 {    
     int tabla[8][8]={0};
+    afisare(tabla);
     return 0;
 }
